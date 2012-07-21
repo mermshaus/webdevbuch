41ad392d04035cee8250c51da3bdd83f315a53ab
Marc Ermshaus <marc@ermshaus.org>
2012-07-22 01:21:27 +0200

    No need to explicitly specify phpsog-dir in export script.

--- export.sh
+++ export.sh
@@ -3,5 +3,5 @@ rm -r ./export
 mkdir ./export
 touch ./export/.gitignore
 chmod +x ./vendor/phpsog/phpsog/phpsog
-./vendor/phpsog/phpsog/phpsog --phpsog-dir=vendor/phpsog/phpsog --project-dir=data build
+./vendor/phpsog/phpsog/phpsog --project-dir=data build
 
